1518de9dd3f63aad47b3810fbc6b40d69bc95445
Danielito1996 <daniel.imbert96@hotmail.com>
2025-12-19 12:42:54 -0600

    Definicion de Layout Compose-like

---
+++ src/lib/components/layouts/Column.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { Modifier } from "../../core/modifier/Modifier";
+    import { ModifierNone } from "../../core/modifier/Modifier";
+
+    export let modifier: Modifier = ModifierNone;
+</script>
+
+<div
+        class="flex flex-col"
+        style={modifier.toStyle()}
+>
+    <slot />
+</div>
